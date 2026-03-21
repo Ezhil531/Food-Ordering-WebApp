3b6be47dbf9e30b3c2636833c12d91e2267cc170
unknown <ezhilarasik102@gmail.com>
2026-03-21 08:32:05 +0530

    filter completed

--- src/CustomHooks/useRestaurans.js
+++ src/CustomHooks/useRestaurans.js
@@ -3,7 +3,8 @@ import { RESTAURANT_API } from "../Utils";
 
 export const useRestaurants = () => {
   const [restaurants, setRestaurants] = useState([]);
-
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [resTitile, setResTitle] = useState("");
   useEffect(() => {
     fetchRestaurants();
   }, []);
@@ -13,11 +14,24 @@ export const useRestaurants = () => {
       const response = await fetch(RESTAURANT_API);
       const data = await response.json();
       // console.log(data?.data?.cards);
-      setRestaurants(data?.data?.cards);
+      setRestaurants(
+        data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants,
+      );
+      setResTitle(data?.data?.cards[0]?.card?.card?.title);
+      setFilteredRestaurants(
+        data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants,
+      );
     } catch (error) {
       console.error("Error fetching restaurants:", error);
     }
   };
 
-  return { restaurants };
+  return {
+    restaurants,
+    filteredRestaurants,
+    resTitile,
+    setFilteredRestaurants,
+  };
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,30 +1,52 @@
+import { useState } from "react";
 import RestuarantCard from "./RestuarantCard";
 import { useRestaurants } from "../CustomHooks/useRestaurans";
 import { Link } from "react-router-dom";
 
 const Hero = () => {
-  const { restaurants } = useRestaurants();
-  const list =
-    restaurants[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-  console.log(list);
+  const [searchTerm, setSearchTerm] = useState("");
+  const {
+    restaurants,
+    resTitile,
+    filteredRestaurants,
+    setFilteredRestaurants,
+  } = useRestaurants();
+
+  const handleSearch = () => {
+    const filtered = restaurants.filter((restaurant) =>
+      restaurant?.info?.name.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+    setFilteredRestaurants(filtered);
+    setSearchTerm("");
+  };
+
+  // if(filteredRestaurants.length === 0) {
+  //   return ()
+  // }
+
   return (
     <div className="h-screen">
-      {/* <div className="search-section">
-        <h1 className="greetings">Hi, Ezhil</h1>
-        <div className="search-bar">
+      <div className="flex items-center justify-center p-4 relative">
+        <h1 className="absolute left-4 text-2xl font-bold">Hi, Ezhil ✌️</h1>
+        <div className="flex items-center gap-4">
           <input
             type="text"
             placeholder="Search for food..."
-            className="search-input"
+            className="w-[400px] border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <button className="search-button">Search</button>
+          <button
+            className="bg-orange-600 text-white px-4 py-2 rounded-md hover:bg-orange-500 cursor-pointer"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
         </div>
-      </div> */}
-      <h1 className="mt-4 ml-1 font-bold text-4xl">
-        {restaurants[0]?.card?.card?.title}
-      </h1>
-      <div className="flex flex-wrap gap-4 justify-center mt-4">
-        {list?.map((restaurant) => (
+      </div>
+      <h1 className="mt-4 ml-1 font-bold text-4xl">{resTitile}</h1>
+      <div className="flex flex-wrap gap-4 justify-left mt-4">
+        {filteredRestaurants?.map((restaurant) => (
           <Link
             key={restaurant?.info?.id}
             to={`/restaurant/${restaurant?.info?.id}`}
